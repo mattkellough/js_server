01dd246ee62733c4f60ee3fc939cb0aaae1121d9
Matt Kellough <mrk10a@gmail.com>
2016-02-17 10:17:57 -0500

    seans loves mickey mouse

---
+++ public/hello.js
@@ -0,0 +1,176 @@
+// console.log("I'm in a seperate JS file");
+//This is a single line comment
+// console.log("new line of code");
+
+/*
+This is a multi line comment.
+Blah Blah Blah
+*/
+
+// console.log("more code");
+//
+// var input = "asdf"
+// var output = test ? "passed" : "failed" ;
+// console.log(output);
+
+// var value = 3;
+// if (value === 0) {
+//   console.log('zero');
+// } else if (value === 1) {
+//   console.log('one');
+// } else {
+//   console.log('something else');
+// }
+
+
+
+// var x = 0;
+// while (x < 10) {
+//   console.log(x);
+//   x +=1;
+//   if (x > 5) {
+//     break;
+//   }
+// }
+
+// while (true) {
+//   console.log(x);
+//   x++;
+// }
+
+// Ruby break === JS break
+// Ruby next === JS Continue
+// Ruby redo === ???
+
+// for (var i = 0; i < ?; i++) {
+//   console.console.log(i);
+// }
+
+// var arr = ["a", "b", "c", "d", "e"]
+// for (var i =0; i < arr.length; i++) {
+//   console.log(i);
+//   console.log(arr[i]);
+// }
+
+// function some_function (arg) {
+//   console.log("some function");
+//   console.log(arg);
+// }
+
+
+// var named_anon_function = function(arg) {
+//   console.log("anon function");
+//   console.log(arg);
+// }
+
+// function addAll() {
+//   var sum = 0;
+//   for (var i = 0; i < arguments.length; i++) {
+//     sum += arguments[i];
+//   }
+//   return sum;
+// }
+
+
+// function withDefaults(a,b) {
+//   return a + b;
+// }
+
+
+// function evenOrodd(num) {
+//   if ()
+// }
+
+// var my_name = "Emmett Brown"
+// console.log(my_name);
+// console.log("I am ...");
+// console.log(my_name.toUpperCase());
+// my_name.toUpperCase();
+// puts "I am..."
+// puts my_name.upcase
+//
+// journey = "We're going to October 21, 2015.. the FUTURE!!"
+// puts journey.gsub "FUTURE", "PAST"
+
+// var journey = "We're going to October 21, 2015.. the FUTURE!!"
+// console.log(journey.replace("FUTURE", "PAST"));
+
+// var actors = ['Michael Fox', 'Christopher Lloyd', 'Lea Thompson', 'Claudia Wells']
+// for (var i = 0; i < actors.length; i++) {
+//   console.log(actors[i], "is so cool.");
+// }
+//
+// actors.each do |actor|
+//   puts "#{actor} is so cool."
+// end
+
+// number_of_hoverboards = "1"
+// console.log("There are at least", number_of_hoverboards, "hoverboard(s) in 2015");
+// number_of_hoverboards = parseInt("1")
+// number_of_hoverboards -= 1
+// console.log("There are at least", number_of_hoverboards,  "hoverboard(s) in 2015");
+
+// var hoverboards = 0
+//
+// while (hoverboards >= 100) {
+//   console.log("The future rocks!");
+//   hoverboards +=1;
+//   if (hoverboards > 5) {
+//     break;
+//   }
+// }
+
+
+// if hoverboards >= 100
+//   puts "The future rocks!"
+// elsif hoverboards < 100 and hoverboards > 1
+//   puts "The future is pretty cool"
+// elsif hoverboards == 1
+//   puts "Meh"
+// else
+//   puts "The future is boring."
+// end
+
+
+// function greeting_for(name){
+//   return ("Hello, " + (name) + "!")
+// }
+//
+// console.log(greeting_for("Marty"));
+
+// console.log("Let's Go!"); "Let's Go!"
+//
+// 89.times do |speed|
+//   puts "#{speed}mph!"
+//   puts "ZOOOOOM" if speed == 88
+// end
+
+// var hoverboards = 0
+//
+// if (hoverboards >= 100) {
+//   console.log("The future rocks!")}
+// else if (hoverboards < 100 && hoverboards > 1) {
+//   console.log("The future is pretty cool"); }
+// else if (hoverboards == 1) {
+//   console.log("Meh");}
+// else {
+//   console.log("The future is boring.");}
+
+
+// console.log("Let's Go!");
+//
+// for (i = 0; i <= 88; i++)
+// {console.log(i + " mph!");
+// if (i === 88) {console.log("ZOOOOOM");}}
+
+// 89.times do |speed|
+//   puts "#{speed}mph!"
+//   puts "ZOOOOOM" if speed == 88
+// end
+
+
+console.log("Let's Go!");
+
+for (i = 0; i<=88; i++ )
+{console.log(i + " mph!");
+if (i == 88) {console.log("ZOOOOOOM!!");}}
